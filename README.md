8e74a4b52900f8585e8ecec7df7cf49c15228f95
liuyiqi <897473127@qq.com>
2017-04-24 18:23:12 +0800

    Add srt2lrc

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [爬取人人听力网 MP3+LRC](https://github.com/lewis617/practical-node/blob/master/rrting.js)
 
 [获取微博以及评论文字](https://github.com/lewis617/practical-node/blob/master/weiboBackup.js)
+
+[批量转换 srt 文件为 lrc 文件](https://github.com/lewis617/practical-node/blob/master/srt2lrc.js)
---
+++ srt2lrc.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
+var glob = require('glob');
+
+// 更改这个变量为你的 srt 文件目录，运行 node srt2lrc ，然后生成的 lrc 文件也将会保存在这个目录中
+var dirPath = path.join(os.homedir(), 'Downloads', 'C0126.A01.Subtitle');
+
+function srtBlockToLrc(block) {
+    var srtBlockReg = new RegExp("(\\d+)[^\\S\\r\\n]*[\\r\\n]" +
+        "(\\d{2}:\\d{2}:\\d{2},\\d{3,4})[^\\S\\r\\n]*-->[^\\S\\r\\n]*(\\d{2}:\\d{2}:\\d{2},\\d{3,4})[^\\S\\r\\n]*[\\r\\n]" +
+        "([\\s\\S]*)");
+
+    var array = srtBlockReg.exec(block);
+    var timeStart = array[2].slice(3, -1).replace(',', '.'),
+        timeEnd = array[3].slice(3, -1).replace(',', '.'),
+        content = array[4].replace('\n', ' ');
+    return '[' + timeStart + ']' + content + '\n' + '[' + timeEnd + ']\n';
+}
+
+function srtFileToLrc(filePath) {
+    var buffer = fs.readFileSync(filePath);
+    var strIn = buffer.toString();
+    var blocksIn = strIn
+        .replace(/\r\n/g, '\n')
+        .split('\n\n')
+        .filter(function (block) {
+            return block.length > 0;
+        });
+    var blocksOut = blocksIn.map(srtBlockToLrc);
+    var lrcPath = filePath.replace('.srt', '.lrc');
+    fs.writeFile(lrcPath, blocksOut.join(''), function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(lrcPath, ' is done!');
+    });
+}
+
+glob(path.join(dirPath, '*.srt'), function (err, files) {
+    files.forEach(function (file) {
+        srtFileToLrc(file);
+    });
+});
