84852238fec929d22b5115f8f0bb7a5569e4529d
liuyiqi <897473127@qq.com>
2017-04-28 09:45:39 +0800

    Add txt to lrc

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 [批量转换 srt 文件为 lrc 文件](https://github.com/lewis617/practical-node/blob/master/srt2lrc.js)
 
 [批量重命名文件](https://github.com/lewis617/practical-node/blob/master/renameFiles.js)
+
+[批量转换 txt 文件为 lrc 文件(没有普适性)](https://github.com/lewis617/practical-node/blob/master/renameFiles.js)
---
+++ txt2lrc.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
+var glob = require('glob');
+var Iconv = require('iconv').Iconv;
+
+// 更改这个变量为你的文件目录
+var dirPath = path.join(os.homedir(), 'Downloads', '办公室英语口语（MP3+文本）');
+
+function readFileSync_encoding(filename, encoding) {
+    var content = fs.readFileSync(filename);
+    var iconv = new Iconv(encoding, 'UTF-8');
+    var buffer = iconv.convert(content);
+    return buffer.toString('utf8');
+}
+
+function txtFileToLrc(filePath) {
+    var strIn = readFileSync_encoding(filePath, 'gb18030');
+    var arrayOut = strIn
+        .split(/[\u4e00-\u9fa5]/)[0]
+        .replace(/[\r\n]/g, '')
+        .replace('’', '\'')
+        .split(/A:|B:/);
+    var strOut = arrayOut.reduce(function (last, current, index) {
+        var addStr = '';
+        if (index === 0) {
+            addStr = '[00:00.00]';
+        } else if (index === 1) {
+            addStr = '\tA:';
+        } else if (index % 2 === 0) {
+            addStr = '\r\n[00:0' + index + '.00]B:'
+        } else if (index & 1 === 1) {
+            addStr = '\r\n[00:0' + index + '.00]A:'
+        }
+        return last + addStr + current;
+    }, '');
+
+    var lrcPath = filePath.replace('.txt', '.lrc');
+    fs.writeFile(lrcPath, strOut, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(lrcPath, ' is done!');
+    });
+}
+
+glob(path.join(dirPath, '*.txt'), function (err, files) {
+    files.forEach(function (file) {
+        txtFileToLrc(file);
+    });
+});
