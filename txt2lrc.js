ab2f1aeb7f985aa9b78f299f8ca2ab1486f8f051
liuyiqi <897473127@qq.com>
2017-05-10 11:13:35 +0800

    Add mp3-duration to txt2lrc

--- txt2lrc.js
+++ txt2lrc.js
@@ -3,9 +3,10 @@ var path = require('path');
 var os = require('os');
 var glob = require('glob');
 var Iconv = require('iconv').Iconv;
+var mp3Duration = require('mp3-duration');
 
 // 更改这个变量为你的文件目录
-var dirPath = path.join(os.homedir(), 'Downloads', '办公室英语口语（MP3+文本）');
+var dirPath = path.join(os.homedir(), 'Downloads', '求职英语300句');
 
 function readFileSync_encoding(filename, encoding) {
     var content = fs.readFileSync(filename);
@@ -18,46 +19,55 @@ function txtFileToLrc(filePath) {
     var strIn = readFileSync_encoding(filePath, 'gb18030');
 
     try {
-        // var arrayOut = strIn
-        //     .split(/A:|I:|R:|B:/)
-        //     .filter(function (item) {
-        //         return item && !/[\u4e00-\u9fa5]/.test(item)
-        //     });
-
         var arrayOut = strIn
-            .split(/[\u4e00-\u9fa5]/)[0]
-            .replace(/[\r\n]/g, '')
-            .replace(/’/g, '\'')
-            .split(/A:|B:/)
-            .slice(1);
+            .split(/A:|I:|R:|B:/)
+            .filter(function (item) {
+                return item && !/[\u4e00-\u9fa5]/.test(item)
+            });
+
+        // var arrayOut = strIn
+        //     .split(/[\u4e00-\u9fa5]/)[0]
+        //     .replace(/[\r\n]/g, '')
+        //     .replace(/’/g, '\'')
+        //     .split(/A:|B:/)
+        //     .slice(1);
     } catch (err) {
         console.log(err, filePath);
     }
 
-    var lastSec = 0.00;
+    mp3Duration(filePath.replace('.txt', '.mp3'), function (err, duration) {
+        if (err) return console.log(err.message);
 
-    var strOut = arrayOut.reduce(function (last, current, index) {
-        var currentSec = lastSec + (current.length / 15).toFixed(2) * 1;
+        var durationLength = duration/arrayOut.join('').length;
 
-        var sec = (lastSec % 60).toFixed(2);
-        var min = Math.floor(lastSec / 60);
-        var numFormate = function (num) {
-            return num >= 10 ? num : ('0' + num);
-        };
+        var lastSec = 0.00;
 
-        lastSec = currentSec;
-        var minSec = '[' + numFormate(min) + ':' + numFormate(sec) + ']';
-        var addStr = (index > 0 ? '\r\n' : '') + minSec + (index % 2 === 0 ? 'A:' : 'B:');
-        return last + addStr + current.replace(/？/g, '?').replace(/，/g, ',');
-    }, '');
+        var strOut = arrayOut.reduce(function (last, current, index) {
+            var currentSec = lastSec + (current.length *durationLength).toFixed(2) * 1;
 
-    var lrcPath = filePath.replace('.txt', '.lrc');
-    fs.writeFile(lrcPath, strOut, function (err) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log(lrcPath, ' is done!');
+            var sec = (lastSec % 60).toFixed(2);
+            var min = Math.floor(lastSec / 60);
+            var numFormate = function (num) {
+                return num >= 10 ? num : ('0' + num);
+            };
+
+            lastSec = currentSec;
+            var minSec = '[' + numFormate(min) + ':' + numFormate(sec) + ']';
+            var addStr = (index > 0 ? '\r\n' : '') + minSec + (index % 2 === 0 ? 'A:' : 'B:');
+            return last + addStr + current.replace(/？/g, '?').replace(/，/g, ',');
+        }, '');
+
+        var lrcPath = filePath.replace('.txt', '.lrc');
+        fs.writeFile(lrcPath, strOut, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log(lrcPath, ' is done!');
+        });
+
     });
+
+
 }
 
 glob(path.join(dirPath, '*.txt'), function (err, files) {
