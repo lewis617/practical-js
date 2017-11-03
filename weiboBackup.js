00017b717ae1cc29687226ec43d8dae75466e69d
tianke.lyq <tianke.lyq@alibaba-inc.com>
2017-11-03 12:51:36 +0800

    Add IMG alt to weboBackup

--- weiboBackup.js
+++ weiboBackup.js
@@ -7,7 +7,19 @@
 var nodeArray = Array.from(document.querySelectorAll('.WB_text'));
 
 var textArray = nodeArray.map(function (node) {
-    return node.lastChild.nodeValue.replace(/(^：)|(\s*$)|(^\s*)/g, '');
+  var startContent = false;
+  return Array.from(node.childNodes).map(function(childNode){
+    // 过滤评论人
+    if(childNode.nodeName==='#text' && childNode.nodeValue.match(/^：/)) startContent = true;
+  
+    // 判断是否开始评论内容
+    if(startContent){
+      // 文字的情况
+      if(childNode.nodeName==='#text') return childNode.nodeValue.replace(/(^：)|(\s+$)|(^\s+)/g, '');
+      // 图片表情的情况
+      else if(childNode.nodeName === 'IMG') return childNode.alt;
+    }
+  }).join('');
 });
 
 console.log(textArray.join('\n'));
