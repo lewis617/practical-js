a7b6f3d9f11816352f4c932bee93a1fab81cc63d
tianke.lyq <tianke.lyq@alibaba-inc.com>
2017-11-03 19:13:51 +0800

    remove startContent

--- weiboBackup.js
+++ weiboBackup.js
@@ -4,21 +4,16 @@
  * 然后将下面的js拷贝到浏览器的console面板中
  */
 
-var nodeArray = Array.from(document.querySelectorAll('.WB_text'));
+var nodeArray = Array.from(document.querySelectorAll('.list_con .WB_text'));
 
 var textArray = nodeArray.map(function (node) {
-  var startContent = false;
   return Array.from(node.childNodes).map(function(childNode){
-    // 过滤评论人
-    if(childNode.nodeName==='#text' && childNode.nodeValue.match(/^：/)) startContent = true;
-  
-    // 判断是否开始评论内容
-    if(startContent){
-      // 文字的情况
-      if(childNode.nodeName==='#text') return childNode.nodeValue.replace(/(^：)|(\s+$)|(^\s+)/g, '');
-      // 图片表情的情况
-      else if(childNode.nodeName === 'IMG') return childNode.alt;
-    }
+    // 文字的情况
+    if(childNode.nodeName==='#text') return childNode.nodeValue.replace(/(\s+$)|(^\s+)/g, '');
+    // 图片表情的情况
+    else if(childNode.nodeName === 'IMG') return childNode.alt;
+    // 链接的情况
+    else if(childNode.nodeName === 'A') return childNode.lastChild.nodeValue;
   }).join('');
 });
 
